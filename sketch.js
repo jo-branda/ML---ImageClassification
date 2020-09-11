705ee69c126b688e8168d7ec98917b905b765f7d
João Brandão <55622690+jo-branda@users.noreply.github.com>
2020-09-11 15:50:52 +0100

    InputLabel Added

--- sketch.js
+++ sketch.js
@@ -1,72 +1,73 @@
 var mobilenet;
-var predictor;
+var classifier;
 var tucano;
 var video;
-var label = '';
+var label = "";
 var value = 0;
 
-// Best buttons in the market 
+var inputLabel;
+// Best buttons in the market
 var bt_train;
-var slider;
 var bt_add;
 
-function modelReady(){
-    console.log('Model is ready!');
+function modelReady() {
+  console.log("Model is ready!");
 }
 
-function videoReady(){
-    console.log('Video is ready!');
+function videoReady() {
+  console.log("Video is ready!");
 }
 
-function whileTraining(loss){
-    if(loss == null){
-        console.log('Training Complete');
-        predictor.predict(gotResults);
-    }else{
-        console.log(loss);
-    }
+function whileTraining(loss) {
+  if (loss == null) {
+    console.log("Training Complete");
+    classifier.classify(gotResults);
+  } else {
+    console.log(loss);
+  }
 }
 
-function gotResults(error,results){
-    if(error){
-        console.error(error);
-    }else{
-        console.log(results.value);
-        value = results.value;
-        predictor.predict(gotResults);
-        //var prob = results[0].confidence;
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    label = results[0].label;
+    classifier.classify(gotResults);
+    //var prob = results[0].confidence;
 
-        // createP(label);
-        // createP(prob);
-    }
+    // createP(label);
+    // createP(prob);
+  }
 }
-function setup(){
-    createCanvas(640,550);
-    video = createCapture(VIDEO);
-    video.hide();
-    background(0);
-    mobilenet = ml5.featureExtractor('MobileNet', modelReady);
-    predictor = mobilenet.regression(video, videoReady);
+function setup() {
+  createCanvas(640, 550);
+  video = createCapture(VIDEO);
+  video.hide();
+  background(0);
+  mobilenet = ml5.featureExtractor("MobileNet", modelReady);
+  classifier = mobilenet.classification(video, videoReady);
 
-    slider = createSlider(0 ,1, 0.5, 0.01);
+  inputLabel = createInput();
 
-    bt_add = createButton('Add Image');
-    bt_add.mousePressed(function(){
-        predictor.addImage(slider.value());
-    });
+  bt_add = createButton("Add Image");
+  bt_add.mousePressed(function () {
+    var ImageLabel = inputLabel.value();
+    classifier.addImage(ImageLabel);
+  });
 
-    bt_train = createButton('Train Model');
-    bt_train.mousePressed(function(){
-        console.log("Training...");
-        predictor.train(whileTraining);
-    });
-}
-
-function draw(){
-    background(0);
-    image(video, 0, 0);
-    fill(255);
-    textSize(32);
-    text(value, 10, height - 20);
+  bt_train = createButton("Train Model");
+  bt_train.mousePressed(function () {
+    console.log("Training...");
+    label = "Training...";
+    classifier.train(whileTraining);
+  });
 }
 
+function draw() {
+  background(0);
+  image(video, 0, 0);
+  fill(255);
+  textSize(32);
+  text(label, 10, height - 20);
+}
